5030e080d0c6d681e88c2c1c38ceff135c383e6f
Fox028 <0000109479353XSP@al.educacao.sp.gov.br>
2024-05-29 17:12:18 -0300

    Create Sketch.js

---
+++ Sketch.js
@@ -0,0 +1,54 @@
+ //variaveisdaBolinha
+let xBolinha = 300
+let yBolinha = 200
+let diametro = 30
+
+//velocidadedaBolinha
+let velocidadexBolinha = 5
+let velocidadeyBolinha = 5
+
+//variaveisdaRaquete
+let xRaquete = 5 
+let yRaquete = 150
+let baseRaquete = 10
+let alturaRaquete = 90
+
+function setup() {
+  createCanvas(600, 400);
+  }
+
+function draw() {
+  background(0);
+  mostraBolinha();
+  velocidadeBolinha();
+  colisaoBorda()
+  mostraRaquete()
+  movimentoRaquete()
+}
+  
+  function mostraRaquete(){
+  rect(xRaquete,yRaquete,baseRaquete,alturaRaquete)
+  }
+
+function movimentoRaquete(){
+   if(KeyIsDown(UP_ARROW))
+  yRaquete -= 10
+  if(KeyIsDown(DOWN_ARROW))
+    yRaquete += 10
+}
+
+function mostraBolinha(){
+  circle(xBolinha,yBolinha,diametro)
+  }
+
+function velocidadeBolinha(){
+  xBolinha += velocidadexBolinha
+  yBolinha += velocidadeyBolinha
+  }
+
+function colisaoBorda(){
+   if(xBolinha + 15 > width || xBolinha - 15 < 0)
+     velocidadexBolinha*= -1
+  if(yBolinha + 15 > height || yBolinha - 15 < 0)
+    velocidadeyBolinha *= -1
+}
